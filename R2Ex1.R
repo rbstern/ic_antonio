8f34b1dc3f3e05dce6cffd0deaf046fcdd41821b
Guilherme <gui.anto@hotmail.com>
2018-09-25 06:53:49 -0300

    Exercicios

---
+++ R2Ex1.R
@@ -0,0 +1,22 @@
+metropolis = function(B = 10^5)
+{
+  theta = rep(NA, B)
+  theta[1] = rcauchy(1) #rnorm(1, 0, 1)
+  g = function(theta) dcauchy(theta) * dnorm(2, theta, 1)
+  for(ii in 2:B)
+  {
+    theta_prop = theta[ii-1] + rcauchy(1) #rnorm(1, 0, 1)
+    L = g(theta_prop)/g(theta[ii-1])
+    theta[ii] = theta_prop
+    if(runif(1, 0, 1) > L) theta[ii] = theta[ii-1]
+  }
+  theta
+}
+
+theta = metropolis()
+
+mean(theta)
+mean(theta^2) - mean(theta)^2
+
+
+#Provavelmente x|theta é N(theta,1) , não N(0,1)
---
+++ R2Ex2.R
@@ -0,0 +1,21 @@
+metropolis = function(B = 10^5)
+{
+  theta = rep(NA, B)
+  theta[1] = rgamma(1, 1, 1) #abs(rnorm(1, 0, 1)) ?
+  g = function(theta) dgamma(theta,1,1) * dexp(0.5, theta)
+  for(ii in 2:B)
+  {
+    theta_prop = theta[ii-1] + rgamma(1, 1, 1) #abs(rnorm(1, 0, 1)) ?
+    L = g(theta_prop)/g(theta[ii-1])
+    theta[ii] = theta_prop
+    if(runif(1, 0, 1) > L) theta[ii] = theta[ii-1]
+  }
+  theta
+}
+
+theta = metropolis()
+
+mean(theta)
+mean(theta^2) - mean(theta)^2
+
+#Variancia e media muito altas.
